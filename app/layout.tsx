8ad1e6ab691cd0e58cf09a126aba543e255b3871
marmiju <mar.miju.dev@gmail.com>
2025-06-03 18:51:20 +0600

    Create Project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { montserrat } from "@/lib/Font";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }

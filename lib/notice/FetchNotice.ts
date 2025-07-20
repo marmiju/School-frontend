81ea77d0ac3a21fd503fc77c0c74761729f9c815
azizar rahman <mar.miju.dev@gmail.com>
2025-07-20 15:58:51 +0600

    notice

---
+++ lib/notice/FetchNotice.ts
@@ -0,0 +1,23 @@
+export interface Notice {
+  id: number;
+  title: string;
+  link: string;
+}
+
+
+const baseurl = process.env.Url  || 'http://localhost:3000'
+
+
+export default async function FetchNotice(){
+
+    try {
+        const res =await fetch(`${baseurl}/data/notice.json`)
+
+        const result:Notice[] = await res.json()
+        return await result
+        
+    } catch (error) {
+        new Error('Something Went Wrong!')
+    }
+
+} 
